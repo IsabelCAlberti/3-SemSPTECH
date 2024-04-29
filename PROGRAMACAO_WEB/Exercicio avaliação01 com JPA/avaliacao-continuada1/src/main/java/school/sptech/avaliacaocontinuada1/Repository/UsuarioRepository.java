72563b616c19f1efb09972cfe7d1043e556f3c49
Isabel Alberti <125396002+IsabelCAlberti@users.noreply.github.com>
2024-04-29 18:32:52 -0300

    Delete PROGRAMACAO_WEB directory

--- PROGRAMACAO_WEB/Exercicio avaliação01 com JPA/avaliacao-continuada1/src/main/java/school/sptech/avaliacaocontinuada1/Repository/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package school.sptech.avaliacaocontinuada1.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.sptech.avaliacaocontinuada1.Domain.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> { }
